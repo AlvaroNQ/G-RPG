d908eefa7d1f2f16c3baeee977daeb5bbcd9da3a
Navaqui <74022048+AlvaroNavaqui@users.noreply.github.com>
2021-09-22 00:21:01 +0200

    Update main.cpp

--- Sources/src/main.cpp
+++ Sources/src/main.cpp
@@ -1,1 +1,94 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include "../include/RenderWindow.hpp"
+#include "../include/Entity.hpp"
 
+#define HEIGHT  32
+#define WIDTH   64
+
+using namespace std;
+
+int main(int argc, char* args[])
+{
+	if (SDL_Init(SDL_INIT_VIDEO) > 0)
+		std::cout << "HEY.. SDL_Init HAS FAILED. SDL_ERROR: " << SDL_GetError() << std::endl;
+
+	if (!(IMG_Init(IMG_INIT_PNG)))
+		std::cout << "IMG_init has failed. Error: " << SDL_GetError() << std::endl;
+
+	RenderWindow window("GAME v1.0", 1280, 720);
+
+	SDL_Texture* grassTexture = window.loadTexture("src/gfx/ground_grass_1.png");
+	SDL_Texture* skyTexture = window.loadTexture("src/gfx/sky_1.bmp");
+
+    Entity entities[HEIGHT][WIDTH];
+
+    //std::vector<Entity> entitiees = {Entity(Vector2f(0, 0), grassTexture),
+    //                      			 Entity(Vector2f(33, 0), grassTexture),
+    //                      			 Entity(Vector2f(33, 33), grassTexture),
+    //                      			 Entity(Vector2f(33, 66), grassTexture)};
+
+	bool gameRunning = true;
+
+	SDL_Event event;
+
+	while (gameRunning)
+	{
+		// Get our controls and events
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+				gameRunning = false;
+		}
+
+		window.clear();
+
+        string FileName = "map00";
+        char buf[1000];
+        FILE *Myfile;
+        FileName = "C:/Users/alvar/Desktop/C++/Demo/src/worlds/" + FileName + ".txt";
+
+        char FileNameDraft[FileName.size()+1];
+        strcpy(FileNameDraft, FileName.c_str());
+        Myfile = fopen(FileNameDraft, "r");
+
+        int i=0;
+        while (fgets(buf,1000, Myfile)!=NULL)
+        {
+            for(int j=0 ; j<WIDTH ; j++)
+            {
+                if(buf[j]=='x'){
+                    entities[i][j].Entity2(Vector2f(16 * j, 16 * i), grassTexture);
+                    std::cout << "x ";
+                }else{
+                    entities[i][j].Entity2(Vector2f(16 * j, 16 * i), skyTexture);
+                    std::cout << ". ";
+                }
+            }
+            std::cout << endl;
+            i++;
+        }
+
+        fclose(Myfile);
+
+        window.render(entities[0][0]);
+
+        for(int i=0 ; i<HEIGHT ; i++){
+            for(int j=0 ; j<WIDTH ; j++){
+    			window.render(entities[i][j]);
+            }
+        }
+
+
+		window.display();
+
+	}
+
+	window.cleanUp();
+	SDL_Quit();
+
+	return 0;
+}
